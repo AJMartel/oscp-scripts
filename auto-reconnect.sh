ce0921b7154eb44c13a3e2bad4ffaf621e01b401
root <root@localhost.localdomain>
2016-05-10 20:33:16 -0400

    OpenVPN auto reconnect script

---
+++ auto-reconnect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#!/bin/bash
+while [ "true" ]
+do
+    VPNCON=$(nmcli con | grep tap0)
+    if [[ $VPNCON ]]; then
+        VPNCON=$(echo $VPNCON | cut -f 1 -d " ")
+    else
+        echo "No connection, attempting to (re)connect"
+        openvpn #/path/to/connection.conf
+    fi
+    sleep 30 
+done
